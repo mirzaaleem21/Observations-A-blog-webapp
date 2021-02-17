279b2085fa4fd8ff84d267988358096f7866423a
unknown <abdulaleemfaizan@gmail.com>
2021-02-17 21:00:15 +0530

    Added nodemon

--- app.js
+++ app.js
@@ -119,10 +119,8 @@ app.post("/", async(req,res)=>{
 
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+const port = process.env.PORT || 3000;
+
 
 
 app.listen(port, function() {
